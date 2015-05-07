21c2642e35ad57d72896d5360e446c44661219a4
Daniel Amaya <daniel@thegameclash.com>
2015-05-06 22:48:06 -0700

    come on man.

--- lib/Pegex/Nginx/Data.pm
+++ lib/Pegex/Nginx/Data.pm
@@ -5,23 +5,7 @@ extends 'Pegex::Tree';
 use Data::Dumper;
 use feature 'say';
 
-#sub got_conf { $_[1][0] }
-#sub got_block { +{map @$_, map @$_, @{(pop)}} }
-#$VAR1 = [
-#          [
-#            'cock',
-#            undef,
-#            ' / '
-#          ],
-#          [
-#            [
-#              'cook',
-#              ' ass'
-#            ]
-#          ]
-#        ];
-
 sub got_block { 
-  +{map $_, map $_, @{(pop)}}
+  +{map $_, @_}
 }
 1;
